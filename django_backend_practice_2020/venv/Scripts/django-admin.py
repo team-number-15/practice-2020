fee3478f6e701b54af4737437031a7dc40d370f4
Valentyn Shcherban <valentain.sav99@gmail.com>
2020-04-23 22:08:56 +0300

    Removed deprecated files

--- django_backend_practice_2020/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\dima1\PycharmProjects\django_backend_practice_2020\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
