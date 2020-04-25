280c068f958aa083d61553365ae36b6ba71ba6ca
Shevchenko Olga <olga.shevchenko277@gmail.com>
2020-04-25 18:17:05 +0300

    Added new model for general info about speed test

--- django_backend_practice_2020/connection_speed_measurement/models.py
+++ django_backend_practice_2020/connection_speed_measurement/models.py
@@ -2,39 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# General speed information
 class SpeedTest(models.Model):
-<<<<<<< HEAD
-    test_id = models.fields.UUIDField(primary_key=True, default=models.fields.uuid.uuid4, editable=False)
-    tester = models.ForeignKey(User, related_name='test_created_by')
-=======
-    test_id = models.fields.CharField(help_text='Unique identifier for test', max_length=32)
+    test_id = models.fields.AutoField(primary_key=True, help_text='Unique identifier for test', max_length=32)
     tester = models.ForeignKey(User, on_delete=models.DO_NOTHING)
->>>>>>> bugfix/models_pk
+    file = models.fields.BinaryField(help_text="Binary data for speed testing")
     file_size_mb = models.fields.FloatField(help_text="Size of data for speed testing in megabytes")
-
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        return self.test_id
 
 
 # Test unit
-class SpeedTestUnit(SpeedTest):
+class SpeedTestUnit(models.Model):
     unit_id = models.fields.UUIDField(primary_key=True, editable=False)
+    test_id = models.ForeignKey(SpeedTest, on_delete=models.DO_NOTHING)
     begin_timestamp = models.fields.DateTimeField(help_text="Timestamp of test beginning")
-    file = models.fields.BinaryField(help_text="Binary data for speed testing")
     packet_count = models.fields.IntegerField(help_text="Number of packets in test")
     packet_number = models.fields.IntegerField(help_text="Order number of packet in test")
 
 
 # Test result
-class SpeedTestResult(SpeedTest):
+class SpeedTestResult(models.Model):
     result_id = models.fields.UUIDField(primary_key=True, editable=False)
-<<<<<<< HEAD
-    unit_id = models.ForeignKey(SpeedTestUnit)
-=======
     unit_id = models.ForeignKey(SpeedTestUnit, on_delete=models.DO_NOTHING)
->>>>>>> bugfix/models_pk
     duration = models.fields.DurationField(help_text="Time between current timestamp and timestamp of test beginning")
     speed = models.fields.FloatField(help_text="Speed of connection for current test unit in mbit/sec")
