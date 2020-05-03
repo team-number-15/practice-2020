d1b5a5b5ed51991d5ab80bf96015faf99f886511
Valentyn Shcherban <valentain.sav99@gmail.com>
2020-05-03 21:00:44 +0300

    Made some minor changes for urls

--- django_backend_practice_2020/authentication/urls.py
+++ django_backend_practice_2020/authentication/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import GoogleLogin, UserList
 
 urlpatterns = [
     path('social/google/', GoogleLogin.as_view(), name='google_login'),
-    path('users/', UserList.as_view(), name='users_list')
+    path('users/', UserList.as_view(), name='users_list'),
+    path('', include('rest_auth.urls')),
 ]
