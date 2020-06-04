e4f7a3e7fe3b7a740b38dcb1f53406fa365d183d
Valentyn Shcherban <valentain.sav99@gmail.com>
2020-06-05 01:23:06 +0300

    Postgres installation with ansible

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,42 +1,39 @@
-properties([
-    pipelineTriggers([
-        GenericTrigger(
-            causeString: 'Generic Cause',
-            regexpFilterExpression: '',
-            regexpFilterText: '',
-            token: '71B6B68DFC8C34235B642BE2EBQ12')
-        ])
-    ])
+// properties([
+//     pipelineTriggers([
+//         GenericTrigger(
+//             causeString: 'Generic Cause',
+//             regexpFilterExpression: '',
+//             regexpFilterText: '',
+//             token: '71B6B68DFC8C34235B642BE2EBQ12')
+//         ])
+//     ])
 
 node(){
 
-    String APP_DIR = "practice-2020/django_backend_practice_2020/"
+    String APP_DIR = "practice-2020"
 
-    stage("CHECKOUT"){
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-            doGenerateSubmoduleConfigurations: false, 
-            extensions: [], 
-            submoduleCfg: [], 
-            userRemoteConfigs: [[credentialsId: '6838fe0f-1193-43cb-ae85-9dcb497ed2fb',
-            url: 'https://github.com/Volektyn/spring-boot']]])
-    }
+    // stage("CHECKOUT"){
+    //         checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+    //         doGenerateSubmoduleConfigurations: false, 
+    //         extensions: [], 
+    //         submoduleCfg: [], 
+    //         userRemoteConfigs: [[credentialsId: '6838fe0f-1193-43cb-ae85-9dcb497ed2fb',
+    //         url: 'https://github.com/Volektyn/spring-boot']]])
+    // }
     
-    stage(""){
+    // stage("PRE_DEPLOY"){
+    //     if (fileExists(APP_DIR)){
+    //         dir(APP_DIR){
+    //             deleteDir()
+    //         }
+    //     }
 
-    }
+    //     sh "git clone https://github.com/team-number-15/practice-2020"
+
+    //     sh "cd ${APP_DIR}"
+    // }
     
     stage("DEPLOY"){
-
-        if (fileExists("practice-2020")){
-            dir("practice-2020"){
-                deleteDir()
-            }
-        }
-
-        sh "git clone https://github.com/team-number-15/practice-2020"
-
-        sh "cd ${APP_DIR}"
-
         // sh 'virtualenv env -p python3.5'
         // sh '. env/bin/activate'
         // sh 'env/bin/pip install -r requirements.txt'
